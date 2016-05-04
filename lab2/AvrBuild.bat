86fed9740c13e6a6e4c2ec9f3d82f42afe764083
SergeyRevko <s.revko@ya.ru>
2016-05-04 17:44:27 +0300

    fbdg

--- lab2/AvrBuild.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "D:\AVR_projects\lab2\labels.tmp" -fI -W+ie -C V2E -o "D:\AVR_projects\lab2\lab2.hex" -d "D:\AVR_projects\lab2\lab2.obj" -e "D:\AVR_projects\lab2\lab2.eep" -m "D:\AVR_projects\lab2\lab2.map" "D:\AVR_projects\lab2\lab2.asm"
